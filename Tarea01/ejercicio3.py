29fd533c635d3c06bf5f537ca253e36d89a34326
cesarideeo123 <cesarideeo123@gmail.com>
2022-03-31 17:27:53 -0600

    ejercicio 1

--- Tarea01/ejercicio3.py
+++ Tarea01/ejercicio3.py
@@ -1,9 +1,23 @@
-def isPalindrome(cadena):
-    aux = (cadena.lower()).replace(" ","")
-    reversa_cadena = ''.join(reversed(aux))
+def invierte_cadena(cadena):
+    cadenaInvertida = ""
+    for i in cadena:
+        cadenaInvertida = i + cadenaInvertida
+    return cadenaInvertida
+
+def quita_espacios(cadena):
+    cadena_sin_espacios = ''
+    for i in cadena:
+        if(i != " "):
+            cadena_sin_espacios = cadena_sin_espacios + i
+    return cadena_sin_espacios                
+            
+def es_palindromo(cadena):
+    aux = quita_espacios((cadena.lower()))
+    reversa_cadena = invierte_cadena(aux)
     if aux == reversa_cadena:
         print ("La cadena, " + cadena + ", sí es palindromo")
     else:
         print ("La cadena, " + cadena + ", no es palindromo")
 
-isPalindrome("Lavan esa base naval") 
+es_palindromo("Lavan esa base naval")
+
