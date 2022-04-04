6d568dc48b8b107776cdf513d3013414c973124c
cesarideeo123 <cesarideeo123@gmail.com>
2022-04-04 09:50:27 -0500

    tarea terminada

--- Tarea01/ejercicio1.py
+++
@@ -1,8 +0,0 @@
-def sin_repetidos(lista):
-    resultado = []
-    for i in range (0, len(lista)):
-        if lista[i] not in resultado:
-            resultado[i:] = lista[i]
-    print(resultado)
-    
-sin_repetidos(['q','w','a','d','f','d','w','a','f','f','z','q','j'])    
